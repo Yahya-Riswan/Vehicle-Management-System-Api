d54c3b462e6a0076ca6fb00e91c13f35ad51dcfe
Yahya-Riswan <pk.yahyariswan@gmail.com>
2026-02-04 12:46:15 +0530

    Remove all files

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-# Import all models here so SQLAlchemy "sees" them
-from .staff import *
-from .bike import *
-from .bike_images import *
-from .customer import *
-from .bike_expenses import *
-from .general_expenses import * 
-from .sale import *
-# Later you will add: from .bikes import Bike
--- schemas/token.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from .staff import StaffResponse
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-    user: StaffResponse
-
-class TokenData(BaseModel):
-    username: str | None = None
